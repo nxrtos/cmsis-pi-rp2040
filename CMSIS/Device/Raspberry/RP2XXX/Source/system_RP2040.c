9f79a6dd285587e084253b34baed387c85ccbf6d
Chris Hockuba <khockuba@vergewave.com>
2021-02-01 01:29:20 +0100

    Fix SystemInit

--- CMSIS/Device/Raspberry/RP2XXX/Source/system_RP2040.c
+++ CMSIS/Device/Raspberry/RP2XXX/Source/system_RP2040.c
@@ -23,7 +23,7 @@
  * limitations under the License.
  */
 
-#include "RP2040.h"
+#include "RP20XX.h"
 #include "system_RP2040.h"
 #include "cmsis_compiler.h"
 
@@ -56,30 +56,30 @@ void SystemCoreClockUpdate(void)
     uint32_t postdiv2;
 
     switch (clock_source) {
-    case CLK_REF_SRC_ROSC:
-        // We can't directly calculate ROSC frequency, we have to use other stable source
-        // In this case we use known frequency of XOSC
-        CLOCKS->FC0_REF_KHZ  = XOSC_VALUE / 1000;
-        CLOCKS->FC0_MIN_KHZ  = 0;
-        CLOCKS->FC0_MAX_KHZ  = 0;
-        CLOCKS->FC0_INTERVAL = 4;    // 1us * 2 * value
-        // Since this gets more complicated we don't support this source now
-        break;
-    case CLK_REF_SRC_XOSC:
-        ref_div = PLL_SYS->CS & PLL_SYS_CS_REFDIV_Msk;
-        if (PLL_SYS->CS & PLL_SYS_CS_BYPASS_Msk) {
-            SystemCoreClock = XOSC_VALUE / ref_div;
-        } else {
-            // Calculation from datasheet
-            // (FREF / REFDIV) × FBDIV / (POSTDIV1 × POSTDIV2)
-            fb_div          = PLL_SYS->FBDIV_INT;
-            postdiv1        = (PLL_SYS->PRIM & PLL_SYS_PRIM_POSTDIV1_Msk) >> PLL_SYS_PRIM_POSTDIV1_Pos;
-            postdiv2        = (PLL_SYS->PRIM & PLL_SYS_PRIM_POSTDIV2_Msk) >> PLL_SYS_PRIM_POSTDIV2_Pos;
-            SystemCoreClock = (XOSC_VALUE / ref_div) * fb_div / (postdiv1 * postdiv2);
-        }
-        break;
-    case CLK_REF_SRC_AUX:
-        break;
+        case CLK_REF_SRC_ROSC:
+            // We can't directly calculate ROSC frequency, we have to use other stable source
+            // In this case we use known frequency of XOSC
+            CLOCKS->FC0_REF_KHZ  = XOSC_VALUE / 1000;
+            CLOCKS->FC0_MIN_KHZ  = 0;
+            CLOCKS->FC0_MAX_KHZ  = 0;
+            CLOCKS->FC0_INTERVAL = 4;    // 1us * 2 * value
+            // Since this gets more complicated we don't support this source now
+            break;
+        case CLK_REF_SRC_XOSC:
+            ref_div = PLL_SYS->CS & PLL_SYS_CS_REFDIV_Msk;
+            if (PLL_SYS->CS & PLL_SYS_CS_BYPASS_Msk) {
+                SystemCoreClock = XOSC_VALUE / ref_div;
+            } else {
+                // Calculation from datasheet
+                // (FREF / REFDIV) × FBDIV / (POSTDIV1 × POSTDIV2)
+                fb_div          = PLL_SYS->FBDIV_INT;
+                postdiv1        = (PLL_SYS->PRIM & PLL_SYS_PRIM_POSTDIV1_Msk) >> PLL_SYS_PRIM_POSTDIV1_Pos;
+                postdiv2        = (PLL_SYS->PRIM & PLL_SYS_PRIM_POSTDIV2_Msk) >> PLL_SYS_PRIM_POSTDIV2_Pos;
+                SystemCoreClock = (XOSC_VALUE / ref_div) * fb_div / (postdiv1 * postdiv2);
+            }
+            break;
+        case CLK_REF_SRC_AUX:
+            break;
     }
 
     return;
@@ -97,16 +97,30 @@ void SystemInit(void)
 #endif
 
     // We only want to set XOSC to run on our board
-    if (CLOCKS->CLK_REF_CTRL != CLK_REF_SRC_XOSC) {
-        // First bypass PLL
-        PLL_SYS->CS |= PLL_SYS_CS_BYPASS_Msk;
-        while (!(PLL_SYS->CS & PLL_SYS_CS_LOCK_Msk)) {
-            __NOP();
-        }
-
+    //if (CLOCKS->CLK_REF_CTRL != CLK_REF_SRC_XOSC) {
+        // Setup XOSC range
+        XOSC->CTRL = 0xaa0; // 1_15Mhz
+        // Setup startup delay
+        uint32_t startup_delay = (((12 * 1000000) / 1000) + 128) / 256;
+        XOSC->STARTUP = startup_delay;
+        // Enable XOSC
+        XOSC->CTRL = (XOSC->CTRL & ~XOSC_CTRL_ENABLE_Msk) | (0xFAB << XOSC_CTRL_ENABLE_Pos);
+        while (!(XOSC->STATUS & XOSC_STATUS_ENABLED_Msk));
+        
+        // // First bypass PLL
+        // RESETS->RESET |= RESETS_RESET_pll_sys_Msk;
+        // RESETS->RESET &= ~RESETS_RESET_pll_sys_Msk;
+        // while (!(RESETS->RESET_DONE & RESETS_RESET_DONE_pll_sys_Msk));
+        // PLL_SYS->PWR = 0x0;
+        // PLL_SYS->CS |= PLL_SYS_CS_BYPASS_Msk;
+        // while (!(PLL_SYS->CS & PLL_SYS_CS_BYPASS_Msk));
         // Change from using PLL to CLK_REF
-        CLOCKS->CLK_SYS_CTRL &= ~CLOCKS_CLK_SYS_CTRL_SRC_Msk;
-    }
+        CLOCKS->CLK_REF_CTRL = 0x2 << CLOCKS_CLK_REF_CTRL_SRC_Pos;
+
+        MODIFY_REG(CLOCKS->CLK_SYS_CTRL, CLOCKS_CLK_SYS_CTRL_SRC_Msk, 0);
+        MODIFY_REG(CLOCKS->CLK_SYS_CTRL, CLOCKS_CLK_SYS_CTRL_AUXSRC_Msk, 0x03 << CLOCKS_CLK_SYS_CTRL_AUXSRC_Pos);
+        MODIFY_REG(CLOCKS->CLK_SYS_CTRL, CLOCKS_CLK_SYS_CTRL_SRC_Msk, 0x01 << CLOCKS_CLK_SYS_CTRL_SRC_Pos);
+    //}
 
     SystemCoreClockUpdate();
 }
